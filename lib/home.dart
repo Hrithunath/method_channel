8b67bd16d75c138c23a8f119d6f31cef331cdb73
Hrithunath <hrithunath777@gmail.com>
2025-01-11 20:35:38 +0530

    battery level completed

---
+++ lib/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  // Get battery level.
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _getBatteryLevel,
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                child: const Text('Get Battery level'),
+              ),
+              Text(_batteryLevel)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
